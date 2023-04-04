0b8dc47a3ec2a50ca0df94d389335745e743550b
Pratik67796 <pratiksuthar808@gmail.com>
2023-04-04 22:19:28 +0530

    Update school-dashboard.blade.php

--- resources/views/user/dashboard/school-dashboard.blade.php
+++ resources/views/user/dashboard/school-dashboard.blade.php
@@ -185,7 +185,6 @@ School Dashboard
                  <a href="{{ route('course-buy') }}">
                     <img src="{{asset('course_images/').'/'.$course['image']}}" alt="">
                  </a>
-                 
                  <div class="course__tag">
                     <a href="#" class="pink">BESTSELLER</a>
                  </div>
